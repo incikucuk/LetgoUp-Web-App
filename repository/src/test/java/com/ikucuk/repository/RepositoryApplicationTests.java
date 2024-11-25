1c9852c8e710fe17862aacc241fa5c6c703edbd9
ikucuk <incikucuk98@gmail.com>
2024-11-26 00:28:44 +0300

    first commit

---
+++ repository/src/test/java/com/ikucuk/repository/RepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikucuk.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
